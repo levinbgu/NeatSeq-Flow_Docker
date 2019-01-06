69efae8fdd0887045d85a8057ad429eab8819068
levinbgu <levinl@post.bgu.ac.il>
2019-01-06 18:01:37 +0200

    NeatSeq-Flow Docker

---
+++ docker_sge_init.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+
+YOURQ=all.q
+MASTER_HOST_SLOTS=$(nproc)
+PE=shared
+
+# get stored SGE hostname
+export SGE_HOST=`cat /opt/sge/default/common/act_qmaster`
+
+# rename files from SGE_HOST to HOSTNAME
+find /opt/sge/ -name "*$SGE_HOST*" | sed -e "p;s/$SGE_HOST/$HOSTNAME/" | xargs -n2 mv
+
+# replace SGE_HOST text in files
+grep -Rl "$SGE_HOST" /opt/sge/ | xargs sed -i "s/$SGE_HOST/$HOSTNAME/g"
+
+# restart SGE
+/etc/init.d/sgemaster.docker-sge restart
+/etc/init.d/sgeexecd.docker-sge restart
+
+qconf -aattr queue slots [$SGE_HOST=$MASTER_HOST_SLOTS] $YOURQ
+qconf -aattr queue pe_list $PE $YOURQ
+
+# Update NeatSeq-Flow
+source activate NeatSeq_Flow_Tutorial
+pip install git+https://github.com/bioinfo-core-BGU/neatseq-flow.git
+pip install git+https://github.com/bioinfo-core-BGU/neatseq-flow-modules.git
+source deactivate
+
+
+# Update NeatSeq-Flow GUI
+source activate NeatSeq_Flow_GUI
+pip install git+https://github.com/bioinfo-core-BGU/NeatSeq-Flow-GUI.git
+#source deactivate
