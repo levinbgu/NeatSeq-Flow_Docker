2f52fd944d17fdf823f21404a61b01f2d4af6adb
levinbgu <levinl@post.bgu.ac.il>
2019-10-24 11:27:44 +0300

    Update update_NeatSeqFlow.sh

--- update_NeatSeqFlow.sh
+++ update_NeatSeqFlow.sh
@@ -3,9 +3,9 @@
 CONDA='/usr/local/bin/'
 # Update NeatSeq-Flow
 source $CONDA/activate NeatSeq_Flow
-pip install --no-deps git+https://github.com/bioinfo-core-BGU/neatseq-flow.git
-pip install --no-deps git+https://github.com/bioinfo-core-BGU/neatseq-flow-modules.git
-pip install --no-deps git+https://github.com/bioinfo-core-BGU/NeatSeq-Flow-GUI.git
+pip install --upgrade --no-deps git+https://github.com/bioinfo-core-BGU/neatseq-flow.git
+pip install --upgrade --no-deps git+https://github.com/bioinfo-core-BGU/neatseq-flow-modules.git
+pip install --upgrade --no-deps git+https://github.com/bioinfo-core-BGU/NeatSeq-Flow-GUI.git
 #source /opt/conda/bin/deactivate
 
 #source deactivate
