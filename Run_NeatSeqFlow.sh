777182edd7eba255c715e020a0c2a70d92927346
levinbgu <levinl@post.bgu.ac.il>
2020-09-03 22:03:56 +0300

    Update Run_NeatSeqFlow.sh

--- Run_NeatSeqFlow.sh
+++ Run_NeatSeqFlow.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-source activate NeatSeq_Flow
-NeatSeq_Flow_GUI.py --Server --PORT 49190 --UNLOCK_USER_DIR
+su sgeadmin -c 'cd /home/sgeadmin/ && source activate NeatSeq_Flow && NeatSeq_Flow_GUI.py --Server --PORT 49190 --UNLOCK_USER_DIR'
