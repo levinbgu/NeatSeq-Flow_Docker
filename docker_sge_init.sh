d8f8482c818548a8e2fa336524b79f7627b3c6e4
levinbgu <levinl@post.bgu.ac.il>
2021-11-03 09:31:27 +0200

    Update docker_sge_init.sh

--- docker_sge_init.sh
+++ docker_sge_init.sh
@@ -4,6 +4,7 @@
 YOURQ=all.q
 MASTER_HOST_SLOTS=$(nproc)
 HOME=/root
+pgrep -x "sge_qmaster"
 if ! pgrep -x "sge_qmaster" >/dev/null ; then
   cd $SGE_ROOT
   ./inst_sge.sh -m -x -s -auto /root/sge_auto_install.conf;
