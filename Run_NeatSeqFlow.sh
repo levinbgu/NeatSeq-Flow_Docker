c328a9386a4baee7ea43dfbba2e3813af2d7b821
levinbgu <levinl@post.bgu.ac.il>
2021-11-03 10:42:43 +0200

    Update Run_NeatSeqFlow.sh

--- Run_NeatSeqFlow.sh
+++ Run_NeatSeqFlow.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # su sgeadmin -l -c 'cd /home/sgeadmin/ && source activate NeatSeq_Flow && NeatSeq_Flow_GUI.py --Server --PORT 49190 --UNLOCK_USER_DIR'
-source /home/miniconda2/bin/activate NeatSeq_Flow
+source activate NeatSeq_Flow
 
 echo 'N' | NeatSeq_Flow_GUI.py \
     --Server \
