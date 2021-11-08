9c243b75b18fe364c096bcb3998dc3c65e9c06d6
levinbgu <levinl@post.bgu.ac.il>
2021-11-08 10:19:57 +0200

    Update docker_sge_init.sh

--- docker_sge_init.sh
+++ docker_sge_init.sh
@@ -10,19 +10,34 @@ if ! [ -d $SGE_ROOT/$SGE_CELL ]; then
   ./inst_sge.sh -m -x -s -auto /root/sge_auto_install.conf;
   sed -i "s/HOSTNAME/`hostname`/" $HOME/sge_exec_host.conf;
   /opt/sge/bin/lx-amd64/qconf -au sgeadmin arusers;
+  /opt/sge/bin/lx-amd64/qconf -au root arusers;
   /opt/sge/bin/lx-amd64/qconf -Me $HOME/sge_exec_host.conf;
   /opt/sge/bin/lx-amd64/qconf -Aq $HOME/sge_queue.conf;
   /opt/sge/bin/lx-amd64/qconf -Ap $HOME/pe_shared.conf;
 fi
 
+flag=0
+getent passwd | while IFS=: read -r name password uid gid gecos home shell; do
+  if [[ $flag -eq 0 ]] ; then
+    if [ $name == 'sgeadmin' ] ; then
+        flag=1
+    fi
+  else
+     echo Adding $name to the SGE submission list
+    /opt/sge/bin/lx-amd64/qconf -au $name arusers;
+  fi
+done
+
+
+
 # get stored SGE hostname
 export SGE_HOST=`cat /opt/sge/default/common/act_qmaster`
 
-# rename files from SGE_HOST to HOSTNAME
-find /opt/sge/ -name "*$SGE_HOST*" | sed -e "p;s/$SGE_HOST/$HOSTNAME/" | xargs -n2 mv
+## rename files from SGE_HOST to HOSTNAME
+# find /opt/sge/ -name "*$SGE_HOST*" | sed -e "p;s/$SGE_HOST/$HOSTNAME/" | xargs -n2 mv
 
-# replace SGE_HOST text in files
-grep -Rl "$SGE_HOST" /opt/sge/ | xargs sed -i "s/$SGE_HOST/$HOSTNAME/g"
+## replace SGE_HOST text in files
+# grep -Rl "$SGE_HOST" /opt/sge/ | xargs sed -i "s/$SGE_HOST/$HOSTNAME/g"
 
 # restart SGE
 /etc/init.d/sgemaster.docker-sge restart
