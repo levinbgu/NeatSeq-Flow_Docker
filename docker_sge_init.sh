4dd98df818ca58e3dcd0b9ab963880aa27b0f428
levinbgu <levinl@post.bgu.ac.il>
2021-11-08 12:33:25 +0200

    Update docker_sge_init.sh

--- docker_sge_init.sh
+++ docker_sge_init.sh
@@ -16,19 +16,6 @@ if ! [ -d $SGE_ROOT/$SGE_CELL ]; then
   /opt/sge/bin/lx-amd64/qconf -Ap $HOME/pe_shared.conf;
 fi
 
-flag=0
-getent passwd | while IFS=: read -r name password uid gid gecos home shell; do
-  if [[ $flag -eq 0 ]] ; then
-    if [ $name == 'sgeadmin' ] ; then
-        flag=1
-    fi
-  else
-     echo Adding $name to the SGE submission list
-    /opt/sge/bin/lx-amd64/qconf -au $name arusers;
-  fi
-done
-
-
 
 # get stored SGE hostname
 export SGE_HOST=`cat /opt/sge/default/common/act_qmaster`
@@ -42,5 +29,18 @@ export SGE_HOST=`cat /opt/sge/default/common/act_qmaster`
 # restart SGE
 /etc/init.d/sgemaster.docker-sge restart
 /etc/init.d/sgeexecd.docker-sge restart
+
+
+flag=0
+getent passwd | while IFS=: read -r name password uid gid gecos home shell; do
+  if [[ $flag -eq 0 ]] ; then
+    if [ $name == 'sgeadmin' ] ; then
+        flag=1
+    fi
+  else
+     echo Adding $name to the SGE submission list
+    /opt/sge/bin/lx-amd64/qconf -au $name arusers;
+  fi
+done
 
 /opt/sge/bin/lx-amd64/qconf -aattr queue slots [$SGE_HOST=$MASTER_HOST_SLOTS] $YOURQ
