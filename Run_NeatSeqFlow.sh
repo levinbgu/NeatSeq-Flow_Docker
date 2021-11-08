4bd126afe744ce0e0ce17173c61202fab6e53921
levinbgu <levinl@post.bgu.ac.il>
2021-11-08 10:50:37 +0200

    Update Run_NeatSeqFlow.sh

--- Run_NeatSeqFlow.sh
+++ Run_NeatSeqFlow.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
-# su sgeadmin -l -c 'cd /home/sgeadmin/ &&
-# wget https://raw.githubusercontent.com/bioinfo-core-BGU/NeatSeq-Flow-Using-Docker/master/doc/Tutorial_Parameter_file.yaml &&
-# wget https://raw.githubusercontent.com/bioinfo-core-BGU/neatseq-flow-tutorial/master/Samples_conda.nsfs &&
-# source activate NeatSeq_Flow && NeatSeq_Flow_GUI.py --Server --PORT 49190 --UNLOCK_USER_DIR'
-mkdir /home/sgeadmin/Tutorial
-cd /home/sgeadmin/Tutorial
+su sgeadmin -l -c 'mkdir /home/sgeadmin/Tutorial  &&
+cd /home/sgeadmin/Tutorial &&
 wget https://raw.githubusercontent.com/bioinfo-core-BGU/NeatSeq-Flow-Using-Docker/master/doc/Tutorial_Parameter_file.yaml &&
-wget https://raw.githubusercontent.com/bioinfo-core-BGU/neatseq-flow-tutorial/master/Samples_conda.nsfs &&
+wget https://raw.githubusercontent.com/bioinfo-core-BGU/neatseq-flow-tutorial/master/Samples_conda.nsfs'
+# source activate NeatSeq_Flow && NeatSeq_Flow_GUI.py --Server --PORT 49190 --UNLOCK_USER_DIR'
+
 
 source activate NeatSeq_Flow
 
